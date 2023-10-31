5725087d5a239fb306d1ad85085a4b237caf1a0f
Mikhail Sherkunkov <sherkunkov@gmail.com>
2023-10-31 18:59:01 +0800

    cw 30.10 done

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,9 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define IDC_COMBO 1001
+#define IDC_BUTTON_APPLY 1002
+
 #include<Windows.h>
 #include <cstdio>
-//#include"resource.h"
+#include <vector>
+#include <string>
+
+
+
+CONST CHAR* g_CURSOR[] = { "Busy.ani", "Normal Select.ani", "Working In Background.ani", "Move.ani" };
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "My window"; // Имя класса окна
+
+std::vector<std::string> LoadCursorsFromDir(const std::string& dir);
+
 void GetWindowParams(HWND hwnd);
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -73,10 +84,32 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
+{ 
+	static HCURSOR hCursor;
 	switch (uMsg)
 	{
 	case WM_CREATE:
+	{
+		HWND hCombo = CreateWindowEx
+		(
+			NULL, "ComboBox", "", WS_CHILD | WS_VISIBLE | CBN_DROPDOWN | WS_VSCROLL, 
+			10, 10, 200, 200, hwnd, (HMENU)IDC_COMBO, GetModuleHandle(NULL), NULL
+		);
+		/*for(int i = 0; i < sizeof(g_CURSOR) / sizeof(g_CURSOR[0]); i++)
+		{
+			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)g_CURSOR[i]);
+		}*/
+		std::vector<std::string> cursors = LoadCursorsFromDir("starcraft-original\\*");
+		for(int i = 0; i < cursors.size(); i++)
+		{
+			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)cursors[i].c_str());
+		}
+
+		HWND hButton = CreateWindowEx(
+			NULL, "Button", "Apply", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			220, 10, 100, 24, hwnd, (HMENU)IDC_BUTTON_APPLY, GetModuleHandle(NULL), NULL);
+
+	}
 		break;
 	case WM_MOVE:
 		GetWindowParams(hwnd);
@@ -88,6 +121,26 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		GetWindowParams(hwnd);
 		break;
 	case WM_COMMAND:
+		switch(LOWORD(wParam))
+		{
+		case IDC_BUTTON_APPLY:
+		{
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
+			int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+			CHAR sz_filename[FILENAME_MAX] = {};
+			CHAR sz_filepath[_MAX_PATH] = "starcraft-original\\";
+			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_filename);
+			strcat(sz_filepath, sz_filename);
+			hCursor = (HCURSOR)LoadImage(
+				GetModuleHandle(NULL), sz_filepath, 
+				IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE
+			);
+			SetClassLong(hwnd, GCL_HCURSOR, (LONG)hCursor);
+			SetClassLong(GetDlgItem(hwnd, IDC_BUTTON_APPLY), GCL_HCURSOR, (LONG)hCursor);
+			SetClassLong(GetDlgItem(hwnd, IDC_COMBO), GCL_HCURSOR, (LONG)hCursor);
+		}
+			break;
+		}
 		break;
 	case WM_DESTROY: PostQuitMessage(0); break;
 	case WM_CLOSE: DestroyWindow(hwnd); break;
@@ -108,5 +161,22 @@ void GetWindowParams(HWND hwnd)
 	sprintf(sz_message, "%s [X = %i, Y = %i, Width = %i, Height = %i. Cursor_X = %i, Cursor_Y = %i]",
 		g_sz_WINDOW_CLASS, wp.left, wp.top, Width, Height, p.x, p.y);
 	SetWindowText(hwnd, sz_message);
+}
+
+std::vector<std::string> LoadCursorsFromDir(const std::string& dir)
+{
+	std::vector<std::string> files;
+	WIN32_FIND_DATA data;
+	for(
+		HANDLE hFind = FindFirstFile(dir.c_str(), &data);
+		FindNextFile(hFind, &data);
+		)
+	{
+		if(strcmp(strchr(data.cFileName, '.'), ".cur") == 0 ||
+			strcmp(strchr(data.cFileName, '.'), ".ani") == 0)
+			files.push_back(data.cFileName);
+	}
+	return files;
+
 }
 
