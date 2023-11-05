58d3debb6269ef3a16f7c668ef1499bdbdeb3d82
sherkunkov@gmail.com <sherkunkov@gmail.com>
2023-11-05 23:46:48 +0800

    in progress

---
+++ Calc2/main.cpp
@@ -0,0 +1,306 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<Windows.h>
+#include<stdio.h>
+#include"resource.h"
+
+CONST CHAR g_sz_CLASSNAME[] = "Calc";
+
+CONST INT START_X = 10;
+CONST INT START_Y = 10;
+
+CONST INT BUTTON_SIZE = 50;
+CONST INT INTERVAL = 5;
+CONST INT BUTTON_DOUBLE_SIZE = BUTTON_SIZE * 2 + INTERVAL;
+
+CONST INT SCREEN_WIDTH = (BUTTON_SIZE) * 5 + INTERVAL * 4;
+CONST INT SCREEN_HEIGHT = 20;
+
+CONST INT BUTTON_START_X = START_X;
+CONST INT BUTTON_START_Y = START_Y + SCREEN_HEIGHT + INTERVAL * 2;
+
+CONST CHAR* OPERATIONS[] = { "/", "*", "-", "+" };
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmsLine, INT nCmdShow)
+{
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(wc));
+
+	wc.cbWndExtra = 0;
+	wc.cbClsExtra = 0;
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = 0;
+
+	wc.hIcon = ExtractIconA(hInstance, "calc.ico", 0);
+	wc.hIconSm = ExtractIconA(hInstance, "calc.ico", 0);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+
+	wc.hInstance = hInstance;
+	wc.lpszClassName = g_sz_CLASSNAME;
+	wc.lpszMenuName = NULL;
+	wc.lpfnWndProc = WndProc;
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Class registration failed", "Error", MB_OK);
+		return 0;
+	}
+
+	HWND hwnd = CreateWindowEx
+	(
+		NULL,
+		g_sz_CLASSNAME,
+		g_sz_CLASSNAME,
+		WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME ^ WS_MAXIMIZEBOX,
+		//WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		(BUTTON_SIZE + INTERVAL) * 5 + BUTTON_START_X * 3, SCREEN_HEIGHT + (BUTTON_SIZE + INTERVAL) * 5 + INTERVAL * 2,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, "Window creation failed", "Error", MB_OK);
+		return 0;
+	}
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	MSG msg;
+	while (GetMessage(&msg, 0, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	static DOUBLE a = 0, b = 0;
+	static INT operation = 0;
+	static BOOL input = false;	//����������� ���� �����
+	static BOOL input_operation = false;	//����������� ���� ��������
+	switch (uMsg)
+	{
+	case WM_CREATE:
+	{
+		HWND hEdit = CreateWindowEx
+		(
+			NULL, "Edit", "Screen",
+			WS_CHILDWINDOW | WS_VISIBLE | WS_BORDER | ES_RIGHT | ES_READONLY,
+			START_X, START_Y,
+			SCREEN_WIDTH, SCREEN_HEIGHT,
+			hwnd,
+			(HMENU)IDC_EDIT,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		int digit = 0;
+		char sz_digit[2] = "";
+		for (int i = 6; i >= 0; i -= 3)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				digit++;
+				sz_digit[0] = digit + 48;
+				CreateWindowEx
+				(
+					NULL, "Button", sz_digit,
+					WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+					BUTTON_START_X + (BUTTON_SIZE + INTERVAL) * j,
+					BUTTON_START_Y + (BUTTON_SIZE + INTERVAL) * i / 3,
+					BUTTON_SIZE, BUTTON_SIZE,
+					hwnd,
+					(HMENU)(IDC_BUTTON_0 + digit),
+					GetModuleHandle(NULL),
+					NULL
+				);
+			}
+		}
+		CreateWindowEx
+		(
+			NULL, "Button", "0",
+			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_START_X, BUTTON_START_Y + (BUTTON_SIZE + INTERVAL) * 3,
+			BUTTON_DOUBLE_SIZE, BUTTON_SIZE,
+			hwnd,
+			(HMENU)(IDC_BUTTON_0),
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "Button", ".",
+			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_START_X + BUTTON_DOUBLE_SIZE + INTERVAL, BUTTON_START_Y + (BUTTON_SIZE + INTERVAL) * 3,
+			BUTTON_SIZE, BUTTON_SIZE,
+			hwnd,
+			(HMENU)(IDC_BUTTON_POINT),
+			GetModuleHandle(NULL),
+			NULL
+		);
+
+		for (int i = 0; i < sizeof(OPERATIONS) / sizeof(OPERATIONS[0]); i++)
+		{
+			CreateWindowEx
+			(
+				NULL, "Button", OPERATIONS[i],
+				WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+				BUTTON_START_X + (BUTTON_SIZE + INTERVAL) * 3, BUTTON_START_Y + (BUTTON_SIZE + INTERVAL) * i,
+				BUTTON_SIZE, BUTTON_SIZE,
+				hwnd,
+				(HMENU)(IDC_BUTTON_SLASH - i),
+				GetModuleHandle(NULL),
+				NULL
+			);
+		}
+		CreateWindowEx
+		(
+			NULL, "Button", "<-",
+			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_START_X + (BUTTON_SIZE + INTERVAL) * 4, BUTTON_START_Y,
+			BUTTON_SIZE, BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_BSP,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "Button", "C",
+			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_START_X + (BUTTON_SIZE + INTERVAL) * 4, BUTTON_START_Y + BUTTON_SIZE + INTERVAL,
+			BUTTON_SIZE, BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_CLEAR,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		SendMessage(GetDlgItem(CreateWindowEx
+		(
+			NULL, "Button", "=",
+			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_START_X + (BUTTON_SIZE + INTERVAL) * 4, BUTTON_START_Y + BUTTON_DOUBLE_SIZE + INTERVAL,
+			BUTTON_SIZE, BUTTON_DOUBLE_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_EQUAL,
+			GetModuleHandle(NULL),
+			NULL
+		), IDC_BUTTON_EQUAL), BM_SETIMAGE, IMAGE_ICON, (LPARAM)(ExtractIcon(GetModuleHandleA(NULL), "equal.ico", 0)));
+		
+	}
+	break;
+	case WM_COMMAND:
+	{
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+		//					Digits & Point
+		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9 || LOWORD(wParam) == IDC_BUTTON_POINT)
+		{
+			if (!input)SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
+			input = true;
+			CHAR sz_buffer[MAX_PATH]{};
+			SendMessage(hEdit, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
+			if (strcmp(sz_buffer, "Screen") == 0)
+			{
+				SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
+				sz_buffer[0] = 0;
+			}
+			CHAR sz_digit[2]{};
+			if (LOWORD(wParam) == IDC_BUTTON_POINT)
+			{
+				if (strchr(sz_buffer, '.'))break;
+				sz_digit[0] = '.';
+			}
+			else sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + 48;
+			strcat(sz_buffer, sz_digit);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		//						Operations:
+		if (LOWORD(wParam) >= IDC_BUTTON_PLUS && LOWORD(wParam) <= IDC_BUTTON_SLASH)
+		{
+			if (input)
+			{
+				CHAR sz_buffer[MAX_PATH]{};
+				SendMessage(hEdit, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
+				b = atof(sz_buffer);
+				input = false;
+				if (a == 0)a = b;
+			}
+			if (input_operation)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_EQUAL, 0);
+			operation = LOWORD(wParam);
+			input_operation = true;
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_EQUAL)
+		{
+			if (input)
+			{
+				CHAR sz_buffer[MAX_PATH]{};
+				SendMessage(hEdit, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
+				b = atof(sz_buffer);
+				input = false;
+				if (a == 0)a = b;
+			}
+			switch (operation)
+			{
+			case IDC_BUTTON_PLUS:	a += b;	break;
+			case IDC_BUTTON_MINUS:	a -= b;	break;
+			case IDC_BUTTON_ASTER:	a *= b;	break;
+			case IDC_BUTTON_SLASH:	a /= b;	break;
+			}
+			input_operation = false;
+			CHAR sz_buffer[MAX_PATH]{};
+			sprintf(sz_buffer, "%g", a);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_BSP)
+		{
+			CHAR sz_buffer[MAX_PATH]{};
+			SendMessage(hEdit, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
+			if (sz_buffer[0])sz_buffer[strlen(sz_buffer) - 1] = 0;
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_CLEAR)
+		{
+			a = b = 0;
+			operation = 0;
+			input = false;
+			input_operation = false;
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
+		}
+	}
+	break;
+	case WM_KEYDOWN:
+	{
+		if (GetKeyState(VK_SHIFT) < 0)
+		{
+			if (wParam == 0x38)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_ASTER, 0);
+		}
+		else if (LOWORD(wParam) >= 0x30 && LOWORD(wParam) <= 0x39)
+			SendMessage(hwnd, WM_COMMAND, LOWORD(wParam) - 0x30 + IDC_BUTTON_0, 0);
+		if (LOWORD(wParam) >= 0x60 && LOWORD(wParam) <= 0x69)
+			SendMessage(hwnd, WM_COMMAND, LOWORD(wParam) - 0x60 + IDC_BUTTON_0, 0);
+		switch (LOWORD(wParam))
+		{
+		case VK_OEM_PLUS:	SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_PLUS, 0);	break;
+		case VK_OEM_MINUS:	SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_MINUS, 0); break;
+		case VK_MULTIPLY:	SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_ASTER, 0); break;
+		case VK_OEM_2:
+		case VK_DIVIDE:		SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_SLASH, 0); break;
+		case VK_OEM_PERIOD:	SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_POINT, 0); break;
+		case VK_RETURN:		SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_EQUAL, 0); break;
+		case VK_ESCAPE:		SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_CLEAR, 0); break;
+		case VK_BACK:		SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_BSP, 0); break;
+		}
+	}
+	break;
+	case WM_DESTROY:PostQuitMessage(0); break;
+	case WM_CLOSE:	DestroyWindow(hwnd); break;
+	default:		return DefWindowProc(hwnd, uMsg, wParam, lParam); break;
+	}
+	return NULL;
+}
---
+++ Calc2/resource.h
@@ -0,0 +1,58 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+
+#define IDC_EDIT			999
+#define IDC_BUTTON_0		1000
+#define IDC_BUTTON_1		1001
+#define IDC_BUTTON_2		1002
+#define IDC_BUTTON_3		1003
+#define IDC_BUTTON_4		1004
+#define IDC_BUTTON_5		1005
+#define IDC_BUTTON_6		1006
+#define IDC_BUTTON_7		1007
+#define IDC_BUTTON_8		1008
+#define IDC_BUTTON_9		1009
+
+#define IDC_BUTTON_POINT	1010
+
+#define IDC_BUTTON_PLUS		1011
+#define IDC_BUTTON_MINUS	1012
+#define IDC_BUTTON_ASTER	1013
+#define IDC_BUTTON_SLASH	1014
+#define IDC_BUTTON_EQUAL	1015
+#define IDC_BUTTON_CLEAR	1016
+#define IDC_BUTTON_BSP		1017
+
+#define ICO_EDIT			""
+#define ICO_BUTTON_0		"zero.ico"
+#define ICO_BUTTON_1		"one.ico"
+#define ICO_BUTTON_2		"two.ico"
+#define ICO_BUTTON_3		"three.ico"
+#define ICO_BUTTON_4		"four.ico"
+#define ICO_BUTTON_5		"five.ico"
+#define ICO_BUTTON_6		"six.ico"
+#define ICO_BUTTON_7		"seven.ico"
+#define ICO_BUTTON_8		"eight.ico"
+#define ICO_BUTTON_9		"nine.ico"
+
+#define ICO_BUTTON_POINT	""
+
+#define ICO_BUTTON_PLUS		"plus.ico"
+#define ICO_BUTTON_MINUS	"minus.ico"
+#define ICO_BUTTON_ASTER	""
+#define ICO_BUTTON_SLASH	"divide.ico"
+#define ICO_BUTTON_EQUAL	"equal.ico"
+#define ICO_BUTTON_CLEAR	""
+#define ICO_BUTTON_BSP		""
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
